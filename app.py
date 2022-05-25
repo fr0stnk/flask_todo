c2b4b32d95596f226238b088240a3cea4cfdea13
fr0stnk <nikita.koifman@stratisplatform.com>
2022-05-25 18:39:25 +0200

    feat: add model and index function

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqllite.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.sqlite'
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
-    id = 
+    task_id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-@app.route('/')
+    def __repr__(self) -> str:
+        return '<Task %r>' % self.task_id
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method != 'POST':
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks=tasks)
+    task_content = request.form['content']
+    new_task = Todo(content=task_content)
+
+    try:
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect('/')
+    except Exception:
+        return f'There was an issue adding your task. Exception: {Exception}'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
